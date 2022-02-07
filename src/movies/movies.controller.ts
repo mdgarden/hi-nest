5b0f9faf212c83f02d6c58d8982dc22d3038a82e
Madang Garden <66259746+mdgarden@users.noreply.github.com>
2022-02-08 00:32:15 +0900

    Movies Controller

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,9 +1,29 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Post, Patch } from '@nestjs/common';
 
 @Controller('movies')
 export class MoviesController {
   @Get()
   getAll() {
     return 'This will return all movies';
+  }
+
+  @Get('/:id')
+  getOne(@Param('id') id: string) {
+    return `This will return one movie with the id: ${id}`;
+  }
+
+  @Post()
+  create() {
+    return 'This will create a movie';
+  }
+
+  @Delete('/:id')
+  deleteMovie(@Param('id') movieId: string) {
+    return `This will delete a movie ${movieId}`;
+  }
+
+  @Patch('/:id')
+  patchMovie(@Param('id') movieId: string) {
+    return `This will return one movie with the id: ${movieId}`;
   }
 }
