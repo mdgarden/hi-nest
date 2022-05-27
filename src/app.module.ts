53558ca1b0277672d10f74cb096a1143e1e18309
Madang <chooi9522@gmail.com>
2022-05-27 23:15:22 +0900

    add memo

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { MoviesModule } from './movies/movies.module';
 import { AppController } from './app.controller';
 
+// app.module은 AppController랑 AppProvider만 가지고 있어야함
 @Module({
   imports: [MoviesModule],
   controllers: [AppController],
